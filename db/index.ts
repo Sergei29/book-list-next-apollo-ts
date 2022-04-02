32670605f5fb8482d09cda86e8baf268663fb6fc
Sergejs Basangovs <sergejs.basangovs@gmail.com>
2022-04-02 14:34:58 +0300

    fix: moved prisma into apollo context

--- db/index.ts
+++ db/index.ts
@@ -1,5 +1,6 @@
 import { prisma } from '../prisma/prisma'
 import { Note } from '../src/types'
+import { PrismaClient } from '@prisma/client'
 
 export const getAllNotes = async () => await prisma.notes.findMany()
 
@@ -23,3 +24,24 @@ export const updateNote = async ({ id, ...restNoteData }: Note) =>
     where: { id },
     data: { ...restNoteData },
   })
+
+export const generateDataSource = (prisma: PrismaClient) => ({
+  getAllNotes: async () => await prisma.notes.findMany(),
+  getNoteById: async (id: string) =>
+    await prisma.notes.findUnique({
+      where: { id },
+    }),
+  addNewNote: async (noteData: Note) =>
+    await prisma.notes.create({
+      data: noteData,
+    }),
+  deleteNoteById: async (id: string) =>
+    await prisma.notes.delete({
+      where: { id },
+    }),
+  updateNote: async ({ id, ...restNoteData }: Note) =>
+    await prisma.notes.update({
+      where: { id },
+      data: { ...restNoteData },
+    }),
+})
--- pages/api/graphql/index.ts
+++ pages/api/graphql/index.ts
@@ -1,14 +1,22 @@
 import { ApolloServer } from 'apollo-server-micro'
 import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
-import { resolvers } from './resolvers'
-import { typeDefs } from './schema'
 import Cors from 'micro-cors'
+import { resolvers } from '../../../apollo-server/resolvers'
+import { typeDefs } from '../../../apollo-server/schema'
+import { createContext } from '../../../apollo-server/context'
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
 
 const cors = Cors()
 
 const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
+  context: createContext,
   plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
 })
 
@@ -19,14 +27,9 @@ export default cors(async function handler(req, res) {
     res.end()
     return false
   }
+
   await startServer
   await apolloServer.createHandler({
     path: '/api/graphql',
   })(req, res)
 })
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-}
