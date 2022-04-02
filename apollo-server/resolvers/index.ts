fb35a220ac8ecba7eee9b38b2b7bea5c64deeefc
Sergejs Basangovs <sergejs.basangovs@gmail.com>
2022-04-02 14:41:34 +0300

    skipe: replaced prisma with mocjk data

--- apollo-server/resolvers/index.ts
+++ apollo-server/resolvers/index.ts
@@ -3,7 +3,7 @@ import { IResolver } from '../../src/types'
 
 export const resolvers: IResolver = {
   Query: {
-    getNotes: (parent, args, ctx, info) => ctx.dataSource.getAllNotes(),
+    getNotes: (parent, args, ctx, info) => [{ id: '62473c65d234343674689442', title: 'My 1st note title' }], // ctx.dataSource.getAllNotes(),
     getNoteById: (parent, args, ctx, info) => {
       const foundNote = ctx.dataSource.getNoteById(args.id)
       if (!foundNote) {
@@ -15,21 +15,21 @@ export const resolvers: IResolver = {
   Mutation: {
     addNote: (parent, args, ctx, info) => {
       try {
-        const newNote = ctx.dataSource.addNewNote(args.note)
+        const newNote = { id: '62473c65d234343674689442', title: 'My 1st note title' } //ctx.dataSource.addNewNote(args.note)
         return newNote
       } catch (error) {
         throw new ApolloError('Failed to add new note')
       }
     },
     deleteNoteById: (parent, args, ctx, info) => {
-      const deletedId = ctx.dataSource.deleteNoteById(args.id)
-      if (!deletedId) {
+      const deletedNote = { id: '62473c65d234343674689442', title: 'My 1st note title' } //ctx.dataSource.deleteNoteById(args.id)
+      if (!deletedNote) {
         throw new ApolloError("Note doesn't exist")
       }
-      return deletedId
+      return deletedNote
     },
     updateNote: (parent, args, ctx, info) => {
-      const updatedNote = ctx.dataSource.updateNote(args.note)
+      const updatedNote = { id: '62473c65d234343674689442', title: 'My 1st note title' } // ctx.dataSource.updateNote(args.note)
       if (!updatedNote) {
         throw new ApolloError("Note doesn't exist")
       }
