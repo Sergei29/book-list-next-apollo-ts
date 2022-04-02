e4f7d13481f6348f7b94e338ceb395bec4ad56dd
Sergejs Basangovs <sergejs.basangovs@gmail.com>
2022-04-02 12:47:30 +0300

    fix: apollo client uri updated to relative

--- src/apollo/apolloClient.ts
+++ src/apollo/apolloClient.ts
@@ -3,6 +3,7 @@ import { ApolloClient, InMemoryCache, InMemoryCacheConfig } from '@apollo/client
 const cacheConfig: InMemoryCacheConfig = {}
 
 export const apolloClient = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_GRAPHQL_URI,
+  // uri: process.env.NEXT_PUBLIC_GRAPHQL_URI,
+  uri: '/api/graphql',
   cache: new InMemoryCache(cacheConfig),
 })
