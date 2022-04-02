0f12e6228d87a45838cf7a20025c939879fa57e0
Sergejs Basangovs <sergejs.basangovs@gmail.com>
2022-04-02 14:58:02 +0300

    refactor: final cleanup

--- db/index.ts
+++ db/index.ts
@@ -1,29 +1,5 @@
-import { prisma } from '../prisma/prisma'
 import { Note } from '../src/types'
 import { PrismaClient } from '@prisma/client'
-
-export const getAllNotes = async () => await prisma.notes.findMany()
-
-export const getNoteById = async (id: string) =>
-  await prisma.notes.findUnique({
-    where: { id },
-  })
-
-export const addNewNote = async (noteData: Note) =>
-  await prisma.notes.create({
-    data: noteData,
-  })
-
-export const deleteNoteById = async (id: string) =>
-  await prisma.notes.delete({
-    where: { id },
-  })
-
-export const updateNote = async ({ id, ...restNoteData }: Note) =>
-  await prisma.notes.update({
-    where: { id },
-    data: { ...restNoteData },
-  })
 
 export const generateDataSource = (prisma: PrismaClient) => ({
   getAllNotes: async () => await prisma.notes.findMany(),
