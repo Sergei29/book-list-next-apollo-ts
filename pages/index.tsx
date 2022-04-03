c926679f3dbe9e7cac6116f35d521505c3cbfefa
Sergejs Basangovs <sergejs.basangovs@gmail.com>
2022-04-03 13:43:17 +0300

    test: server-side fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,23 +4,23 @@ import { Typography } from '@mui/material'
 import { apolloClient } from '../apollo/client'
 import { GET_BOOKS } from '../apollo/client'
 import { useQuery } from '@apollo/client'
-// import { BookType } from '../types'
+import { BookType } from '../types'
 
 type Props = {
-  // data: { books?: BookType[] }
+  data: { books?: BookType[] }
   url?: string
 }
 
-const Home: NextPage<Props> = ({ url }) => {
+const Home: NextPage<Props> = ({ url, data }) => {
   console.log('graphql api url: ', url)
 
-  // console.log('data.books', data.books)
+  console.log('data.books', data.books)
 
-  const { data, loading, error } = useQuery(GET_BOOKS)
+  // const { data, loading, error } = useQuery(GET_BOOKS)
 
-  useEffect(() => {
-    console.log('data, loading, error: ', '\n', data, '\n', loading, '\n', error)
-  }, [data, loading, error])
+  // useEffect(() => {
+  //   console.log('data, loading, error: ', '\n', data, '\n', loading, '\n', error)
+  // }, [data, loading, error])
 
   return (
     <div>
@@ -40,15 +40,15 @@ const Home: NextPage<Props> = ({ url }) => {
 }
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  // const { data, error } = await apolloClient.query({
-  //   query: GET_BOOKS,
-  // })
+  const { data, error } = await apolloClient.query({
+    query: GET_BOOKS,
+  })
 
   const url = process.env.NEXT_PUBLIC_GRAPHQL_URI
 
   return {
     props: {
-      // data,
+      data,
       url,
     },
   }
